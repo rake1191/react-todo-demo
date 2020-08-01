77e338d37ad1dba6428590caa2fdfd0d0ad5f42b
Rakesh C <rakeshc@amazon.com>
2020-08-02 01:18:38 +0530

    Updates

---
+++ precache-manifest.519b4842437c2288c9afa9701681fbf8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e525331bbd49d8d5ee3b79fd59028191",
+    "url": "/react-todo-demo/index.html"
+  },
+  {
+    "revision": "a864d40342a25dbb5da7",
+    "url": "/react-todo-demo/static/css/main.64584ac1.chunk.css"
+  },
+  {
+    "revision": "ca8f13278476e6b325a8",
+    "url": "/react-todo-demo/static/js/2.6b922673.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo-demo/static/js/2.6b922673.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a864d40342a25dbb5da7",
+    "url": "/react-todo-demo/static/js/main.2b242dcf.chunk.js"
+  },
+  {
+    "revision": "21158bd8e46d1f776eaf",
+    "url": "/react-todo-demo/static/js/runtime-main.79fe6b03.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-todo-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
